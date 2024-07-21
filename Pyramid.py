614e3c37fdc04a2f20ae683b93d27854c5fcb41f
harshit1951 <harshitpatel1951@gmail.com>
2024-07-21 20:43:36 +0530

    Addition of Pyramid.py file by harshit1951
    Added the file as a part of Practicing the Forking of a Repository.

---
+++ Pyramid.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if j <= 5-i:
+            print(" ",end="") # end="" prevents line break 
+        else:
+            print("+ ",end="") 
+
+    print() #will create line break
+
+    
